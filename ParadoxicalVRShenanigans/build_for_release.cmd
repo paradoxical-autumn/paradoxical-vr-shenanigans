98a4b56cbd2d5c4bf8eea13f0b3e6f44e9a89b89
paradoxical-autumn <110392755+paradoxical-autumn@users.noreply.github.com>
2024-11-22 17:49:45 +0000

    and i missed a flag

--- ParadoxicalVRShenanigans/build_for_release.cmd
+++ ParadoxicalVRShenanigans/build_for_release.cmd
@@ -2,4 +2,4 @@
 set /P "appver=Enter app version (X.Y.Z): "
 echo on
 dotnet publish -c Release --self-contained -r win-x64 -o .\publish
-vpk pack -u ParadoxicalVRShenanigans %appver% -p .\publish -e ParadoxicalVRShenanigans.exe
+vpk pack -u ParadoxicalVRShenanigans -v %appver% -p .\publish -e ParadoxicalVRShenanigans.exe
