68451486c69a159307255bc4a714c2ab7cadc561
paradoxical-autumn <110392755+paradoxical-autumn@users.noreply.github.com>
2025-04-17 18:08:48 +0100

    start work on new build for release script

--- ParadoxicalVRShenanigans/BuildForRelease.py
+++ ParadoxicalVRShenanigans/BuildForRelease.py
@@ -1,16 +1,20 @@
-PACK_ID = "ParadoxicalVRShenanigans"
-PACK_NAME = "Paradoxical VR Shenanigans"
-PACK_ICON = "..\Icons\icon.ico"
-PACK_AUTHORS = "paradoxical autumn"
+import dotenv
+
+PACK_ID = r"ParadoxicalVRShenanigans"
+PACK_NAME = r"Paradoxical VR Shenanigans"
+PACK_ICON = r"..\Icons\icon.ico"
+PACK_AUTHORS = r"paradoxical autumn"
 
 import os
 
-response = os.system("choice /M \"Are you sure you want to start building?\"")
+response = os.system("choice /M \"are you sure you wanna continue?\"")
 
 if response == 2:
     exit()
 
-with open("ParadoxicalVRShenanigans.csproj", "r") as fp:
+os.system('\"C:/Program Files/Microsoft Visual Studio/2022/Community/MSBuild/Current/Bin/MSBuild.exe\" -p:Configuration=Release;OutputPath=./publish')
+
+with open("version.cs", "r") as fp:
     lines = fp.readlines()
 
 target_attr_line = ""
@@ -20,9 +24,11 @@ for line in lines:
         target_attr_line = line
 
 target_attr_line = target_attr_line.strip()
-target_attr_line = target_attr_line.lstrip("<AssemblyVersion>")
-target_attr_line = target_attr_line.rstrip("</AssemblyVersion>")
+target_attr_line = target_attr_line.lstrip('[assembly: AssemblyVersion("')
+target_attr_line = target_attr_line.rstrip('")]')
 
-os.system("dotnet publish -c Release --self-contained -r win-x64 -o .\publish")
+print(target_attr_line)
+
 os.system("vpk download github --repoUrl https://github.com/paradoxical-autumn/paradoxical-vr-shenanigans")
-os.system(f"vpk pack -u \"{PACK_ID}\" -v {target_attr_line} --packTitle \"{PACK_NAME}\" --icon \"{PACK_ICON}\" --packAuthors \"{PACK_AUTHORS}\" -p .\publish -e {PACK_ID}.exe")
+os.system(f"vpk pack -u \"{PACK_ID}\" -v {target_attr_line} --packTitle \"{PACK_NAME}\" --icon \"{PACK_ICON}\" --packAuthors \"{PACK_AUTHORS}\" -p ./publish -e {PACK_ID}.exe")
+# os.system(r"vpk upload github --repoUrl")
