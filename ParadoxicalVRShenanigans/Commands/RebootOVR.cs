30ca9d8cae261d4a3134d3f0d7b5116d8d5b2155
paradoxical-autumn <110392755+paradoxical-autumn@users.noreply.github.com>
2025-04-17 12:37:44 +0100

    create debug command to reboot OVR

---
+++ ParadoxicalVRShenanigans/Commands/RebootOVR.cs
@@ -0,0 +1,25 @@
+﻿using Spectre.Console;
+using Spectre.Console.Cli;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ParadoxVrTools.Commands;
+public class RebootOVRCommand : AsyncCommand
+{
+    public override async Task<int> ExecuteAsync([NotNull] CommandContext context)
+    {
+        AnsiConsole.Clear();
+
+        if(!Utils.IsElevated())
+        {
+            Logger.Log("failed to reboot ovr: not sudo");
+            AnsiConsole.MarkupLine($"[red]{Locale.Errors.RequiresElevation}[/]");
+            return 1;
+        }
+
+        Logger.Log("rebooting OVR...");
+        Utils.RebootOVR();
+
+        return 0;
+    }
+}
