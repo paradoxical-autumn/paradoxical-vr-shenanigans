076553ea77a3b01fbe874f9da087b797a61287fa
paradoxical-autumn <110392755+paradoxical-autumn@users.noreply.github.com>
2025-04-18 12:53:38 +0100

    wow builds are much easier

--- ParadoxicalVRShenanigans/BuildForRelease.py
+++ ParadoxicalVRShenanigans/BuildForRelease.py
@@ -1,4 +1,5 @@
 import dotenv
+from datetime import datetime, timezone
 
 PACK_ID = r"ParadoxicalVRShenanigans"
 PACK_NAME = r"Paradoxical VR Shenanigans"
@@ -11,30 +12,19 @@ dotenv.load_dotenv()
 
 response = os.system("choice /M \"are you sure you wanna continue?\"")
 
+assigned_time = datetime.now(timezone.utc)
+time_ver_str = fr"{assigned_time.year}.{assigned_time.month}{assigned_time.day}.{assigned_time.hour}{assigned_time.minute}"
+print(f"using assigned time: {time_ver_str}")
+os.system("dotnet publish -c Release --self-contained -r win-x64 -o ./publish")
+
 if response == 2:
     exit()
 
-os.system('\"C:/Program Files/Microsoft Visual Studio/2022/Community/MSBuild/Current/Bin/MSBuild.exe\" -p:Configuration=Release;OutputPath=./publish')
-
-with open("version.cs", "r") as fp:
-    lines = fp.readlines()
-
-target_attr_line = ""
-
-for line in lines:
-    if "AssemblyVersion" in line:
-        target_attr_line = line
-
-target_attr_line = target_attr_line.strip()
-target_attr_line = target_attr_line.lstrip('[assembly: AssemblyVersion("')
-target_attr_line = target_attr_line.rstrip('")]')
-
-print(f"found version {target_attr_line}")
 response = os.system("choice /M \"do you wanna continue to packing and uploading?\"")
 
 if response == 2:
     exit()
 
 os.system("vpk download github --repoUrl https://github.com/paradoxical-autumn/paradoxical-vr-shenanigans")
-os.system(f"vpk pack -u \"{PACK_ID}\" -v {target_attr_line} --packTitle \"{PACK_NAME}\" --icon \"{PACK_ICON}\" --packAuthors \"{PACK_AUTHORS}\" -p ./publish -e {PACK_ID}.exe")
+os.system(f"vpk pack -u \"{PACK_ID}\" -v {time_ver_str} --packTitle \"{PACK_NAME}\" --icon \"{PACK_ICON}\" --packAuthors \"{PACK_AUTHORS}\" -p ./publish -e {PACK_ID}.exe")
 os.system(rf"vpk upload github --repoUrl https://github.com/paradoxical-autumn/paradoxical-vr-shenanigans --token {os.environ['GITHUB_TOKEN']}")
