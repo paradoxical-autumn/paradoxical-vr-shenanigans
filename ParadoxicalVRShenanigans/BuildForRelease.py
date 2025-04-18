d69c8b3b3109246a344ea4457b81a8735c9161ce
paradoxical-autumn <110392755+paradoxical-autumn@users.noreply.github.com>
2025-04-18 13:04:47 +0100

    optimise builder

--- ParadoxicalVRShenanigans/BuildForRelease.py
+++ ParadoxicalVRShenanigans/BuildForRelease.py
@@ -1,30 +1,32 @@
 import dotenv
 from datetime import datetime, timezone
+import os
 
 PACK_ID = r"ParadoxicalVRShenanigans"
 PACK_NAME = r"Paradoxical VR Shenanigans"
 PACK_ICON = r"..\Icons\icon.ico"
 PACK_AUTHORS = r"paradoxical autumn"
 
-import os
+def prompt(question: str):
+    response = os.system(f"choice /M \"{question}\" ")
+
+    if response == 2:
+        exit()
 
 dotenv.load_dotenv()
 
-response = os.system("choice /M \"are you sure you wanna continue?\"")
+prompt("compile?")
 
 assigned_time = datetime.now(timezone.utc)
 time_ver_str = fr"{assigned_time.year}.{assigned_time.month}{assigned_time.day}.{assigned_time.hour}{assigned_time.minute}"
 print(f"using assigned time: {time_ver_str}")
 os.system("dotnet publish -c Release --self-contained -r win-x64 -o ./publish")
 
-if response == 2:
-    exit()
-
-response = os.system("choice /M \"do you wanna continue to packing and uploading?\"")
-
-if response == 2:
-    exit()
+prompt("pack?")
 
 os.system("vpk download github --repoUrl https://github.com/paradoxical-autumn/paradoxical-vr-shenanigans")
 os.system(f"vpk pack -u \"{PACK_ID}\" -v {time_ver_str} --packTitle \"{PACK_NAME}\" --icon \"{PACK_ICON}\" --packAuthors \"{PACK_AUTHORS}\" -p ./publish -e {PACK_ID}.exe")
+
+prompt("upload as a draft?")
+
 os.system(rf"vpk upload github --repoUrl https://github.com/paradoxical-autumn/paradoxical-vr-shenanigans --token {os.environ['GITHUB_TOKEN']}")
